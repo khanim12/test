75ab31988766f2eb96dcaccb559f1c4357d02450
Khanim Abdullayeva <khanim.abdullayeva@aist.group>
2025-01-09 16:46:04 +0400

    refactor

--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { UserType } from "../types/userType";
 
-export const fetchUser = async (): Promise<UserType[]> => {
+export const fetchUser = async(): Promise<UserType[]> => {
   const { data } = await axios.get<UserType[]>(
     "https://jsonplaceholder.typicode.com/users"
   );
